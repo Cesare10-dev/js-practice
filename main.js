7b6c230ca20195421ef4e0aa217914648be5509d
Cesare10-dev <cesare1087@gmail.com>
2023-10-27 01:10:03 +0200

    object

--- main.js
+++ main.js
@@ -2,11 +2,22 @@
 
 const array = [1, 2, 3, 4, 5];
 array.push(9);
-console.log(array);
 
 const newArray = new Array(1, 2, 3, 5, 4);
-console.log(newArray);
 
 const copyArray = [...array];
 copyArray.push(8);
-console.log(copyArray);
+
+const arrayTest = [1, 2, 3, 4, 5];
+arrayTest.push(9);
+
+const obj = {
+  name: "Cesare",
+  age: 36,
+  status: "single",
+};
+delete obj.status;
+obj.work = "developer";
+const copyObj = Object.entries(obj);
+console.log(obj);
+console.log(copyObj);
